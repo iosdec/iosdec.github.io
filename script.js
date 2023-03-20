3cbc369447b2ae8f12aa77b44a3b00d2905f818c
Declan Land <declan@superbexperience.com>
2023-03-20 09:53:19 +0100

    fixed hiding buttons when form submitted - almost finished

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const inputField = document.getElementById('input-field');
 const attendButton = document.getElementById('attend-button');
 const messageBox = document.getElementById('message-box');
-const presaveButton = document.getElementById('presave-button')
 
-// enable the attend button when there is text in the input field
 inputField.addEventListener('input', () => {
+
   if (inputField.value) {
     attendButton.classList.remove('disabled');
     attendButton.classList.add('enabled');
@@ -15,16 +14,17 @@ inputField.addEventListener('input', () => {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
+
     inputField.removeAttribute('disabled');
 });
 
 attendButton.addEventListener('click', () => {
 
     if (inputField.value) {
+
       messageBox.classList.add('show');
-      presaveButton.classList.add('show');
-      inputField.style.display = 'none';
-      attendButton.style.display = 'none';
+      inputField.hidden = true;
+      attendButton.hidden = true;
     }
 });
 
@@ -38,13 +38,10 @@ $(document).ready(function() {
       data: $(this).serialize(),
       dataType: 'json',
       success: function() {
-        
-        // Show the message box and presave button
-        $('#message-box').show();
-        $('#presave-button').show();
+
       },
       error: function() {
-        // Handle errors if necessary
+
       }
     });
   });
